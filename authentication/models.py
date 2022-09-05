cc2a3072d618fcfb5ed24fbe12578a4c9fc416c8
Uliana Dzyoba <ulianadzyoba@gmail.com>
2022-09-05 22:02:40 +0300

    creating user profile

---
+++ authentication/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.admin import User
+from phonenumber_field.modelfields import PhoneNumberField
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone_number = PhoneNumberField(unique=True, null=True, blank=True)
+    bio = models.CharField(max_length=500, blank=True)
+    profile_pic = models.ImageField(unique=True, null=True, blank=True, upload_to="images/")
+
+    def __str__(self):
+        return self.user.username
--- chatproject/settings.py
+++ chatproject/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,8 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'phonenumbers',
     'channels',
-    'chatapp'
+    'chatapp',
+    'authentication'
 ]
 
 MIDDLEWARE = [
@@ -116,7 +118,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Istanbul'
 
 USE_I18N = True
 
@@ -127,6 +129,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
