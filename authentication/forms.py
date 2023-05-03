482b76fa218fd2938f42b03cbfb5f211249af1a7
Uliana Dzyoba <ulianadzyoba@gmail.com>
2023-05-03 20:32:36 +0300

    final design

--- authentication/forms.py
+++ authentication/forms.py
@@ -8,7 +8,7 @@ from .models import UserProfile
 class SignUpForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2',)
+        fields = ('username', 'first_name', 'last_name', 'email',)
 
 
 class UserForm(forms.ModelForm):
