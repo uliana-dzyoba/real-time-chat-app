3bc74edf6d2cb957c3f914950bddcafa882cc61d
Uliana Dzyoba <ulianadzyoba@gmail.com>
2023-05-22 15:54:38 +0300

    Update views.py

--- main/views.py
+++ main/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 from rooms.models import Message
 
@@ -7,11 +8,14 @@ default_pfp = 'images/default_avatar.jpg'
 
 NUMBER_OF_MESSAGES = 5
 
+
 # Create your views here.
 def get_frontpage(request):
     if request.user.is_authenticated:
         messages = Message.objects.filter(user=request.user)[0:NUMBER_OF_MESSAGES:-1]
         rooms = set([message.room for message in messages])
+        if not rooms:
+            return redirect(reverse('chats:rooms'))
         chats = []
         for room in rooms:
             message = room.messages.all().first()
